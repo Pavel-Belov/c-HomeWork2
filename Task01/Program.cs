bb9b6819901ea790fc69d6af3956fb04572d6d0b
Pavel Belov <job.pb@yandex.ru>
2023-01-19 12:02:56 +0300

    add home work 2 task 02

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int day = new Random().Next(1, 8);
+Console.Write("Случайно выбранный день недели: ");
+Console.WriteLine(day);
+
+if (day > 5)
+{
+    Console.Write("Является выходным. ");
+
+    if (day == 6) Console.WriteLine("Это суббота");
+    else Console.WriteLine("Это воскресенье");
+}
+else
+{
+    Console.WriteLine("Не является выходным");
+}
